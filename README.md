ab8d3e128e25af14b50ce821660173d7c7cdf7cc
Tania Kolesnik <kolesnik@gmail.com>
2025-10-23 18:55:59 +0100

    add solution

--- README.md
+++ README.md
@@ -14,15 +14,16 @@ Examples:
 - `mv file.txt first_dir/second_dir/file2.txt` create directory `first_dir`
 inside current directory, then create directory `first_dir/second_dir`,
 create file2.txt inside and remove the source file.
+
 ```python
-print(open("file.txt").read())
+print(open("tests/file.txt").read())
 # Some
 # Text
 move_file("mv file.txt first_dir/second_dir/third_dir/file2.txt")
 print(open("first_dir/second_dir/third_dir/file2.txt").read())
 # Some
 # Text
-open("file.txt")
+open("tests/file.txt")
 # FileNotFoundError: [Errno 2] No such file or directory: 'file.txt'
 ```
 
--- app/main.py
+++ app/main.py
@@ -6,13 +6,23 @@ def move_file(command: str) -> None:
     if len(command) != 3:
         return
     mv, source, target = command
+
     if mv != "mv":
         return
 
-    folder_path = os.path.dirname(target)
-    if folder_path:
-        os.makedirs(folder_path, exist_ok=True)
-    with open(source, "r") as s, open(target, "w") as t:
+    source_filename = os.path.basename(source)
+    target_folder = os.path.dirname(target)
+    target_filename = os.path.basename(target)
+
+    if not target_filename:
+        target_filename = source_filename
+
+    if target_folder and not os.path.exists(target_folder):
+        print(f"Creating {target_folder}")
+        os.makedirs(target_folder, exist_ok=True)
+
+    with (open(source, "r") as s,
+          open(os.path.join(target_folder, target_filename), "w") as t):
         for line in s:
             t.write(line)
 
